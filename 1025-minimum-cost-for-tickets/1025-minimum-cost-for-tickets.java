8b033d70ebf00b12d7a3fef5829d08fe460d7594
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2024-12-31 23:02:18 +0530

    Time: 2 ms (45.44%), Space: 42.6 MB (9.08%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -1,20 +1,41 @@
 class Solution {
-    public int mincostTickets(int[] days, int[] costs) {
-        int lastDay=days[days.length-1];
-        int dp[]=new int[lastDay+1];
-        Arrays.fill(dp,0);
+    HashSet<Integer> isTravelNeeded = new HashSet<>();
 
-        int i=0;
-        for(int day=1;day<=lastDay;day++){
-            if(day<days[i]){
-                dp[day]=dp[day-1];
-            }else{
-                i++;
-                dp[day]=Math.min(dp[day-1]+costs[0],
-                Math.min(dp[Math.max(0,day-7)]+costs[1],
-                dp[Math.max(0,day-30)]+costs[2]));
-            }
+    private int solve(int[] dp, int[] days, int[] costs, int currDay) {
+        // If we have iterated over travel days, return 0.
+        if (currDay > days[days.length - 1]) {
+            return 0;
         }
-        return dp[lastDay];
+
+        // If we don't need to travel on this day, move on to next day.
+        if (!isTravelNeeded.contains(currDay)) {
+            return solve(dp, days, costs, currDay + 1);
+        }
+
+        // If already calculated, return from here with the stored answer.
+        if (dp[currDay] != -1) {
+            return dp[currDay];
+        }
+
+        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
+        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
+        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
+
+        // Store the cost with the minimum of the three options.
+        return dp[currDay] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
+    }
+
+    public int mincostTickets(int[] days, int[] costs) {
+        // The last day on which we need to travel.
+        int lastDay = days[days.length - 1];
+        int dp[] = new int[lastDay + 1];
+        Arrays.fill(dp, -1);
+
+        // Mark the days on which we need to travel.
+        for (int day : days) {
+            isTravelNeeded.add(day);
+        }
+
+        return solve(dp, days, costs, 1);
     }
 }

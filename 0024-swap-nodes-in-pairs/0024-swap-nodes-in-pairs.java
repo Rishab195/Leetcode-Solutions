f6c724d5c0b4c30d65da4debd8f0bebda32bfacf
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-01-27 20:04:37 +0530

    Time: 0 ms (100%), Space: 41.4 MB (51.49%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -9,22 +9,20 @@
  * }
  */
 class Solution {
+        public void recur(ListNode head){
+            if(head==null || head.next==null){
+                return;
+            }
+            int temp=head.val;
+            head.val=head.next.val;
+            head.next.val=temp;
+            recur(head.next.next);
+        }
     public ListNode swapPairs(ListNode head) {
         if(head==null || head.next==null){
             return head;
         }
-        ListNode prev=head;
-        ListNode curr=head.next;
-        while(curr!=null){
-            int temp=prev.val;
-            prev.val=curr.val;
-            curr.val=temp;
-            prev=curr.next;
-            if(prev==null){
-                break;
-            }
-            curr=prev.next;
-        }
+        recur(head);
         return head;
     }
 }

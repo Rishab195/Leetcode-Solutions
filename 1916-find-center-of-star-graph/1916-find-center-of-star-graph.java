c447df3349ec6e222888611bb336cbf6ad1d3743
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-01-28 12:05:40 +0530

    Time: 3 ms (33.23%), Space: 73.7 MB (23.12%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.java
@@ -1,29 +1,30 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        // int n=edges.length;
-        // int count=0;
-        // int [] arr=new int[n+2];
-        // for(int i=0;i<n;i++){
-        //     int a=edges[i][0];
-        //     int b=edges[i][1];
-        //     arr[a]+=1;
-        //     arr[b]+=1;
-        // }
-        // for(int i=0;i<arr.length;i++){
-        //     if(arr[i]==n){
-        //         return arr[i];
-        //     }
-        // }
-        // return -1;
-
-        int a=edges[0][0];
-        int b=edges[0][1];
-
-        if(edges[1][0]==a || edges[1][1]==a){
-            return a;
+        int n = edges.length;
+        int[] arr = new int[n + 2]; 
+        for (int i = 0; i < n; i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            arr[a] += 1;
+            arr[b] += 1;
         }
-        else{
-            return b;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == n) {
+                return i;
+            }
         }
+        return -1; 
     }
 }
+
+
+        // int a=edges[0][0];
+        // int b=edges[0][1];
+
+        // if(edges[1][0]==a || edges[1][1]==a){
+        //     return a;
+        // }
+        // else{
+        //     return b;
+        // }

f9d42735596ace942eb182e8f7488471c99d6fec
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-02-19 22:01:59 +0530

    Time: 0 ms (100%), Space: 41.3 MB (78.62%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -6,16 +6,16 @@ class Solution {
             return 0;
         }
         int count=0;
-        for(int i=0;i<n-2;i++){
-           if(nums[i+1]-nums[i]==nums[i+2]-nums[i+1]){
+        for(int i=2;i<n;i++){
+           if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
             count+=1;
-            result+=count;
            }
             else{
+                result+=(count*(count+1))/2;
                 count=0;
             }
         }
-
+    result+=(count*(count+1))/2;
     return result;
         
     }

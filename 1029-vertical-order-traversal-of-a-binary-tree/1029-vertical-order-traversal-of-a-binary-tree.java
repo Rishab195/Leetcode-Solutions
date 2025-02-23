390387647425e2e6289cb32c9ddacc491b8c751b
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-02-23 14:09:19 +0530

    Time: 4 ms (42.15%), Space: 43.2 MB (8.03%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -28,7 +28,7 @@ class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         Queue<Pair> q=new LinkedList<>();
         List<List<Integer>> list=new ArrayList<>();
-        Map<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map=new TreeMap<>();
+        Map<Integer,TreeMap<Integer,List<Integer>>> map=new TreeMap<>();
 
         q.add(new Pair(root,0,0));
         while(!q.isEmpty()){
@@ -37,13 +37,9 @@ class Solution {
             int x=it.row;
             int y=it.col;
            
-            if(!map.containsKey(x)){
-                map.put(x,new TreeMap<>());
-            }
-            if(!map.get(x).containsKey(y)){
-                map.get(x).put(y,new PriorityQueue<>());
-            }
-            map.get(x).get(y).offer(node.val);
+            map.putIfAbsent(x, new TreeMap<>());
+            map.get(x).putIfAbsent(y, new ArrayList<>());
+            map.get(x).get(y).add(node.val);
 
             if(node.left!=null){
                 q.add(new Pair(node.left,x-1,y+1));
@@ -52,13 +48,11 @@ class Solution {
                 q.add(new Pair(node.right,x+1,y+1));
             }
         }
-        for(TreeMap<Integer,PriorityQueue<Integer>> ys: map.values()){
+        for(TreeMap<Integer,List<Integer>> ys: map.values()){
             list.add(new ArrayList<>());
-            for(PriorityQueue<Integer>nodes:ys.values()){
-                while(!nodes.isEmpty()){
-                    System.out.println(nodes.peek());
-                    list.get(list.size()-1).add(nodes.poll());
-                }
+            for(List<Integer>nodes:ys.values()){
+                Collections.sort(nodes);
+                list.get(list.size()-1).addAll(nodes);
             }
         }
         return list;

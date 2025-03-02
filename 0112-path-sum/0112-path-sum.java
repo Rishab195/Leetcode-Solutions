f98817d7f924cc5687d659adbaf2fa75cd6eeacd
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-03-03 01:41:10 +0530

    Time: 2 ms (5.94%), Space: 44.9 MB (6.99%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,16 +15,26 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        int sum=0;
         if(root==null){
             return false;
         }
-        if(root.left==null && root.right==null){
-            return targetSum==root.val;
-        }
-        boolean leftSum=hasPathSum(root.left,targetSum-root.val);
-        boolean rightSum=hasPathSum(root.right,targetSum-root.val);
+        Queue<Pair<TreeNode,Integer>> q=new LinkedList<>();
+        q.offer(new Pair<>(root,targetSum-root.val));
 
-        return leftSum || rightSum;
+        while(!q.isEmpty()){
+            Pair<TreeNode,Integer> curr=q.poll();
+            TreeNode node=curr.getKey();
+            int remSum=curr.getValue();
+            if(node.left==null && node.right==null && remSum==0){
+                return true;
+            }
+            if(node.left!=null){
+                q.offer(new Pair<>(node.left,remSum-node.left.val));
+            }
+            if(node.right!=null){
+                q.offer(new Pair<>(node.right,remSum-node.right.val));
+            }
+        }
+        return false;
     }
 }

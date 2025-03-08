3123ba12f2ac3b5339a32161e3ca41fd7d7fdb0e
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-03-08 16:46:07 +0530

    Time: 353 ms (8.41%), Space: 55.2 MB (5.08%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,23 +1,28 @@
 class Solution {
     public int numSquares(int n) {
-        int []dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return SolveRec(n,dp);
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=1;i*i<=n;i++){
+            arr.add(i*i);
+        }
+        int dp[][]=new int[arr.size()][n+1];
+        for (int[] row : dp) Arrays.fill(row, -1);
+        return SolveRec(n, arr,dp, 0);
     }
-    public int SolveRec(int n, int[]dp){
+    public int SolveRec(int n,ArrayList<Integer>arr,int [][] dp,int index){
         if(n==0){
             return 0;
         }
-        if(n<0){
-            return Integer.MAX_VALUE;
+        if(n<0 || index>=arr.size()){
+            return Integer.MAX_VALUE-1;
         }
-        if(dp[n]!=-1){
-            return dp[n];
+        if(dp[index][n]!=-1){
+            return dp[index][n];
         }
-        int answer=Integer.MAX_VALUE;
-        for(int i=1;i*i<=n;i++){
-            answer=Math.min(answer,1+SolveRec(n-(i*i),dp));
-        }
-        return dp[n]=answer;
+        int take=Integer.MAX_VALUE;
+        if(n>=arr.get(index))
+         take=1+SolveRec(n-arr.get(index),arr,dp,index);
+        int notTake=SolveRec(n,arr,dp,index+1);
+        dp[index][n]=Math.min(take,notTake);
+        return dp[index][n];
     }
 }

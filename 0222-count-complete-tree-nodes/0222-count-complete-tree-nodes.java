53a62f17d452fe3ac48bf57815b56d1e81515762
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-03-18 11:02:27 +0530

    Time: 0 ms (100%), Space: 47.5 MB (76.42%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,25 +14,10 @@
  * }
  */
 class Solution {
-    public int getDepth(TreeNode node){
-        int depth=0;
-        while(node!=null){
-            depth++;
-            node=node.left;
-        }
-        return depth;
-    }
     public int countNodes(TreeNode root) {
         if(root==null){
             return 0;
         }
-        int leftHeight=getDepth(root.left);
-        int rightHeight=getDepth(root.right);
-
-        if(leftHeight==rightHeight){
-            return (1<<leftHeight)+countNodes(root.right);
-        }else{
-            return(1<<rightHeight)+countNodes(root.left);
-        }
+        return 1+countNodes(root.left)+ countNodes(root.right);
     }
 }

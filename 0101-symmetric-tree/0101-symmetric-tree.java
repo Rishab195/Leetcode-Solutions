cc2ed8d5bc810f6b9e95045e84f69e254b34db7a
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-03-18 10:48:36 +0530

    Time: 0 ms (100%), Space: 42 MB (29.91%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -20,16 +20,14 @@ class Solution {
         }
         return isMirror(root.left,root.right);
     }
-    public boolean isMirror(TreeNode left, TreeNode right){
+    public boolean isMirror(TreeNode left,TreeNode right){
         if(left==null && right==null){
             return true;
         }
         if(left==null || right==null){
             return false;
         }
-        if(left.val!=right.val){
-            return false;
-        }
-        return isMirror(left.left,right.right) && isMirror(left.right,right.left);
+        return (left.val==right.val) && isMirror(left.left,right.right) && isMirror(left.right,right.left);
+
     }
 }

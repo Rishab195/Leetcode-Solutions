a09c6d6269814a8a99b1e58b77bbde3a310b747f
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-03-25 23:56:22 +0530

    Time: 8 ms (86.85%), Space: 46.8 MB (38.71%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,37 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals ,(a,b)-> Integer.compare(a[0],b[0]));
-        int n=intervals.length;
-      List<List<Integer>> res=new ArrayList<>();
-      List<Integer> list=new ArrayList<>();
-      list.add(intervals[0][0]);
-      list.add(intervals[0][1]);
-      res.add(list);
-      int first=intervals[0][0];
-      int last=intervals[0][1];
-        int size=0;
-      for(int i=1;i<n;i++){
-        int start=intervals[i][0];
-        int end=intervals[i][1];
-        if(start<=last){
-            res.get(size).set(1,Math.max(last,end));
-            first=res.get(size).get(0);
-            last=res.get(size).get(1);
-        }
-        else{
-            size++;
-            res.add(new ArrayList<>());
-            res.get(size).add(start);
-            res.get(size).add(end);
-            last=end;
-        }
-      }
-    int[][] result = new int[res.size()][2];
-        for (int i = 0; i < res.size(); i++) {
-            result[i][0] = res.get(i).get(0);
-            result[i][1] = res.get(i).get(1);
-        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        List<int[]> res = new ArrayList<>();
+        
+        // Add the first interval
+        int[] lastInterval = intervals[0];
+        res.add(lastInterval);
 
-        return result;
+        for (int i = 1; i < intervals.length; i++) {
+            int start = intervals[i][0], end = intervals[i][1];
+
+            if (start <= lastInterval[1]) { 
+                // Merge intervals by updating the last interval's end time
+                lastInterval[1] = Math.max(lastInterval[1], end);
+            } else {
+                // Add a new interval and update lastInterval reference
+                lastInterval = intervals[i];
+                res.add(lastInterval);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
     }
 }

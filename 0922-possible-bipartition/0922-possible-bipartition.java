1fa0ab13fd9c160346bd6ffb5839f32bdf7bad83
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-03-28 22:06:45 +0530

    Time: 15 ms (72.7%), Space: 51.6 MB (78.31%) - LeetHub

--- 0922-possible-bipartition/0922-possible-bipartition.java
+++ 0922-possible-bipartition/0922-possible-bipartition.java
@@ -1,41 +1,49 @@
-import java.util.*;
+class Pair{
+    int first;
+    int second;
+    public Pair(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
+}
 
 class Solution {
     public boolean possibleBipartition(int n, int[][] dislikes) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
-
-        for (int[] a : dislikes) {
-            int u = a[0];
-            int v = a[1];
+        for(int [] i: dislikes){
+            int u=i[0];
+            int v=i[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-
-        int[] color = new int[n + 1];
-        Arrays.fill(color, -1);
-
-        for (int i = 1; i <= n; i++) {
-            if (color[i] == -1) {
-                if (!fillColor(i, adj, color, 0)) {
+        int[] color=new int[n+1];
+        Arrays.fill(color,-1);
+        for(int i=0;i<=n;i++){
+            if(color[i]==-1){
+                if(bfs(i,0,color,adj)==false){
                     return false;
                 }
             }
         }
         return true;
     }
-
-    private boolean fillColor(int src, ArrayList<ArrayList<Integer>> graph, int[] color, int rang) {
-        color[src] = rang;
-        for (int nbr : graph.get(src)) {
-            if (color[nbr] != -1) {
-                if (color[nbr] == color[src]) {
-                    return false;
+    public boolean bfs(int src,int colorGiven,int []color,ArrayList<ArrayList<Integer>> adj){
+        color[src]=colorGiven;
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(src,colorGiven));
+        while(!q.isEmpty()){
+            int node=q.peek().first;
+            int nodecol=q.peek().second;
+            q.poll();
+            for(int nbr: adj.get(node)){
+                if(color[nbr]==-1){
+                    color[nbr]=1-nodecol;
+                    q.add(new Pair(nbr,color[nbr]));
                 }
-            } else {
-                if (!fillColor(nbr, graph, color, 1 - rang)) {
+                else if(color[nbr]==color[node]){
                     return false;
                 }
             }

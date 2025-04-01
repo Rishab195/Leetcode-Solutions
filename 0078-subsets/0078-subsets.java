839ae1e920734bf0f82e7721a58aa6c10ee31477
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-04-01 11:53:58 +0530

    Time: 0 ms (100%), Space: 42.6 MB (81.77%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,24 +2,24 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> output=new ArrayList<>();
-
         int index=0;
-        Solve(nums,output,index,ans);
+        SolveRec(index,nums,ans,output);
         return ans;
     }
-    public void Solve(int nums[],List<Integer>output,int index,List<List<Integer>> ans){
+    public void SolveRec(int index,int[]nums,List<List<Integer>>ans,List<Integer> output){
         if(index>=nums.length){
-            ans.add(new ArrayList<>(output));
-            return;
-        }
-        // exclude
-        Solve(nums,output,index+1,ans);
+                ans.add(new ArrayList<>(output));
+                return;
+            }
+
+        //exclude
+        SolveRec(index+1,nums,ans,output);
 
         //include
-        int element=nums[index];
-        output.add(element);
-        Solve(nums,output,index+1,ans);
+        output.add(nums[index]);
+        SolveRec(index+1,nums,ans,output);
 
+        //backtrack
         output.remove(output.size()-1);
     }
 }

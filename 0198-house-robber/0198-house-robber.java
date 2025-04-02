1a5a735d15ae333634a70a9d0023d6f7546686c9
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-04-02 22:12:22 +0530

    Time: 0 ms (100%), Space: 40.9 MB (76.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
-       int n=nums.length;
-       int[]dp=new int[n+1];
-       Arrays.fill(dp,-1);
+        int dp[]=new int[nums.length+1];
+        Arrays.fill(dp,-1);
         return solveRec(0,nums,dp);
     }
-    public int solveRec(int n,int[]nums,int[]dp){
-
-        if(n>=nums.length){
+    public int solveRec(int index,int[]nums,int[]dp){
+        int n=nums.length;
+        if(index>=n){
             return 0;
         }
-        if(dp[n]!=-1){
-            return dp[n];
+        if(dp[index]!=-1){
+            return dp[index];
         }
-        int ans=0;
-        int robthisHouse=nums[n]+solveRec(n+2,nums,dp);
-        int skipthisHouse=solveRec(n+1,nums,dp);
 
-        dp[n]=Math.max(robthisHouse,skipthisHouse);
+        //skip
+        int skip=solveRec(index+1,nums,dp);
 
-        return dp[n];
+        //take
+        int take=nums[index]+solveRec(index+2,nums,dp);
+
+        dp[index]= Math.max(skip,take);
+        return dp[index];
     }
 }

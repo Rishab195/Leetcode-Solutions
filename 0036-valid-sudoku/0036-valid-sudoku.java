d8bf8009a753f2102fd4089dc3964217c158c699
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-04-10 10:55:18 +0530

    Time: 1 ms (100%), Space: 45 MB (19.32%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,35 +1,34 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        int n=board.length;
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                char num = board[i][j];
+                char num=board[i][j];
                 if(num!='.' && !isSafe(board,i,j,num)){
-                return false;
+                    return false;
                 }
-            }  
+            }
         }
-         return true;
-
+        return true;
     }
     public boolean isSafe(char[][]board,int row,int col,char number){
-        // row check
-        for(int i=0;i<board.length;i++){
+
+        for(int i=0;i<9;i++){
             if(i!=row && board[i][col]==number){
                 return false;
             }
         }
-        //col check
-        for(int i=0;i<board.length;i++){
-            if(i!=col && board[row][i]==number){
+        for(int j=0;j<9;j++){
+            if(j!=col && board[row][j]==number){
                 return false;
             }
         }
-        //grid check
+
         int sr=(row/3)*3;
         int sc=(col/3)*3;
         for(int i=sr;i<sr+3;i++){
             for(int j=sc;j<sc+3;j++){
-                if((i!=row || j!=col) && board[i][j]==number){
+                if((i != row || j != col) && board[i][j]==number){
                     return false;
                 }
             }

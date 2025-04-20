3992093afc12ee004be2bc0184327fa891fe1be8
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-04-20 22:09:45 +0530

    Time: 13 ms (8.81%), Space: 44.9 MB (5.54%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,15 +5,18 @@ class Solution {
             return "";
         }
         String pref=strs[0];
-        int minlen=Integer.MAX_VALUE;
-        for(int i=0;i<strs.length;i++){
-           int len=strs[i].length();
-             if (len < minlen) {
-                minlen = len;
-                pref = strs[i];
-            }
-        }
-        for(int i=0;i<strs.length;i++){
+        // int minlen=Integer.MAX_VALUE;
+        // for(int i=0;i<strs.length;i++){
+        //    int len=strs[i].length();
+        //      if (len < minlen) {
+        //         minlen = len;
+        //         pref = strs[i];
+        //     }
+        // }
+        for(int i=1;i<strs.length;i++){
+            
+            int minlen = Math.min(pref.length(),strs[i].length());
+
             String newS="";
             for(int j=0;j<minlen;j++){
                 if(strs[i].charAt(j)!=pref.charAt(j)){
@@ -22,9 +25,7 @@ class Solution {
                 newS += strs[i].charAt(j);
             }
             pref = newS;
-            if(pref.length()<minlen){
-                minlen = pref.length();
-            }
+            
         }
         return pref;
     }

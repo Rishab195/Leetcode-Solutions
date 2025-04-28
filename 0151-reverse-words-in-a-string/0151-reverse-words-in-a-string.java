533f93818a21d4793abb407c60a37d2ea82adcf4
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-04-28 12:36:26 +0530

    Time: 9 ms (29.04%), Space: 45.7 MB (7.15%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -4,12 +4,14 @@ class Solution {
         int i=s.length()-1;
         while(i>=0){
             while(i>=0 && s.charAt(i)==' ') i--;
-
-            if(i<0) break;
+            if(i<0){
+                break;
+            }
             int j=i;
-            while(i>=0 && s.charAt(i)!=' ') i--;
-
-            str+=s.substring(i+1,j+1) + " ";
+            while(i>=0 && s.charAt(i)!=' '){
+                i--;
+            }
+            str+=s.substring(i+1,j+1)+ " ";
         }
         return str.trim();
     }

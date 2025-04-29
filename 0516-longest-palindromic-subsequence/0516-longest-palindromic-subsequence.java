b40e9de2fab5996930c20afcf846fbaa29fa826f
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-04-29 11:31:55 +0530

    Time: 56 ms (42.3%), Space: 55.6 MB (14.88%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,33 +1,22 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        StringBuilder s2=new StringBuilder(s);
-        String s3 = s2.reverse().toString();
-        int i=s.length()-1;
-        int j=s3.length()-1;
-
-        int dp[][]=new int[i+1][j+1];
-        for(int k=0;k<=i;k++){
-            for(int l=0;l<=j;l++){
-                dp[k][l]=-1;
-            }
-        }
-        return recur(i,j, s,s3,dp);
-    }
-    public int recur(int i,int j,String text1, String text2,int[][]dp)
-    {
-        if(i<0 || j<0){
+        if(s==null){
             return 0;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        int n=s.length();
+        String str=new StringBuilder(s).reverse().toString();
+
+        int[][]dp=new int[n+1][n+1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(s.charAt(i)==str.charAt(j)){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
         }
-        int count=0;
-        if(text1.charAt(i)==text2.charAt(j)){
-            count=1+recur(i-1,j-1,text1,text2,dp);
-        }
-        else{
-            count=Math.max(recur(i,j-1,text1,text2,dp) , recur(i-1,j,text1,text2,dp));
-        }
-        return dp[i][j]=count;
-}
+        return dp[0][0];
+    }
 }

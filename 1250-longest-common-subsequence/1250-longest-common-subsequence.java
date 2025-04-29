11a4488136b33a9533c6c4d844e85d45435e2eb0
Rishabh jain <119031261+Rishab195@users.noreply.github.com>
2025-04-29 10:45:04 +0530

    Time: 19 ms (87.19%), Space: 50.8 MB (67.41%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -5,13 +5,15 @@ class Solution {
         }
         int m=text1.length();
         int n=text2.length();
+        int ans=0;
         int[][]dp=new int[m+1][n+1];
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 if(text1.charAt(i)==text2.charAt(j)){
-                    dp[i][j]=1+dp[i+1][j+1];
-                }else{
-                    dp[i][j]=Math.max(dp[i][j+1],dp[i+1][j]);
+                dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
                 }
             }
         }
